5e0baaa0fe839a0b49dc6e9d8183dfb5b570f689
Miguel Lara <miguel.larasj@gmail.com>
2023-09-14 18:03:23 -0500

    springboot di initial commit

---
+++ spring_boot_di/src/test/java/com/mikels/dev/spring_boot_di/SpringBootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikels.dev.spring_boot_di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
